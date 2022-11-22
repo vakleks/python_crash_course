08b82f8aedcceaeba1efda9b12f8b19972ae4a4a
ovak <82042917+vakleks@users.noreply.github.com>
2022-11-22 19:19:58 +0200

    add

--- chapter_9_classes/restaurant.py
+++ chapter_9_classes/restaurant.py
@@ -6,6 +6,7 @@ class Restaurant:
         self.name = name.title()
         self.cuisine = cuisine
         self.status = status
+        self.number_served = 0
 
     def describe_restaurant(self):
         """Show info about the restaurant."""
@@ -15,13 +16,21 @@ class Restaurant:
         """Show tha the restaurant is open now."""
         print(f"The {self.name} is {self.status} now.")
 
-mcdonalds = Restaurant('Mc.Donalds', 'american', 'open')
-rukkola = Restaurant('Rukkola', 'italian', 'closed')
-sushiya = Restaurant('Sushiya', 'japanese', 'open')
+    def set_numbers_served(self, number_served):
+        """Show numver served."""
+        self.number_served = number_served
+        print(f"{number_served} numbers served." )
 
-mcdonalds.describe_restaurant()
-mcdonalds.chek_status()
-rukkola.describe_restaurant()
-rukkola.chek_status()
-sushiya.describe_restaurant()
-sushiya.chek_status()
+    def increment_numbers_served(self, add_number):
+        """Add served numbers."""
+        self.number_served += add_number
+
+
+american_restaurant = Restaurant('Mc.Donalds', 'american', 'open')
+italian_restaurant = Restaurant('Rukkola', 'italian', 'closed')
+japanese_restaurant = Restaurant('Sushiya', 'japanese', 'open')
+
+american_restaurant.describe_restaurant()
+american_restaurant.chek_status()
+american_restaurant.set_numbers_served(6)
+
