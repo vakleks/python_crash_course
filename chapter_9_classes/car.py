216ce4f9c40fdd878036a85ee3d62255c8b1276f
ovak <82042917+vakleks@users.noreply.github.com>
2022-11-22 19:28:02 +0200

    update

--- chapter_9_classes/car.py
+++ chapter_9_classes/car.py
@@ -6,12 +6,35 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return formatted meaningful name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
+    def read_odometer(self):
+        """Output message with car mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """Inputodometer's data."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add data to odometer value."""
+        self.odometer_reading +=miles
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
 
