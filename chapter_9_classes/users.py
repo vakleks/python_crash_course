b2049c172f7aa3699488e723546fc9981cbb0381
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-01 12:37:01 +0200

    apdate users

--- chapter_9_classes/users.py
+++ chapter_9_classes/users.py
@@ -7,6 +7,7 @@ class User:
         self.last_name = last_name.title()
         self.age = age
         self.sex = sex
+        self.login_attempts = 0
 
     def create_user(self):
         """Add info about new user."""
@@ -16,9 +17,21 @@ class User:
         """Greeting user."""
         print(f"Hi, {self.first_name}! \nThank you for registration, welcome)")
 
-john_dough = User('john', 'dough', 25, 'male')
-sarrah_konnor = User('sarrah', 'konnor', 38, 'female')
-jedi = User('obivan', 'kennobi', 30, 'male')
+    def increment_login_attempts(self):
+        """Increment the numbers of login attempts."""
+        self.login_attempts += 1
 
+    def reset_login_attempts(self):
+        """Reset login_attempts."""
+        self.login_attempts = 0
+
+sarrah_konnor = User('sarrah', 'konnor', 38, 'female')
 sarrah_konnor.create_user()
 sarrah_konnor.greet_user()
+sarrah_konnor.increment_login_attempts()
+sarrah_konnor.increment_login_attempts()
+sarrah_konnor.increment_login_attempts()
+print(sarrah_konnor.login_attempts)
+sarrah_konnor.reset_login_attempts()
+print(sarrah_konnor.login_attempts)
+
