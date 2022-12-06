b3b7c45710e4f84f52c9b7393530ced5ca3faba5
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-06 11:13:59 +0200

    update dice

--- chapter_9_classes/dice.py
+++ chapter_9_classes/dice.py
@@ -1,13 +1,17 @@
 from random import randint
 
 class Die():
+    """Create dice roll game."""
 
     def __init__(self, sides=6):
+        """Create Die with n-sides."""
         self.sides = sides
 
+    """Return number from 1 to sides."""
     def roll_die(self):
         return randint(1, self.sides)
 
+#create six sides dice
 six_sides = Die()
 results = []
 for roll in range(10):
@@ -16,6 +20,7 @@ for roll in range(10):
 print("\nRoll results of six sides die:")
 print(results)
 
+#create ten sides dice
 ten_sides = Die(sides=10)
 results = []
 for roll in range(10):
@@ -24,6 +29,7 @@ for roll in range(10):
 print("\nRoll results of ten sides die:")
 print(results)
 
+#create twenty sides dice
 twenty_sides = Die(sides=20)
 results = []
 for roll in range(10):
