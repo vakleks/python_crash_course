e8d7e52ec2431d6f7342463fb98632186b5d6bc4
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-06 10:18:46 +0200

    add Skyworker

---
+++ chapter_9_classes/admin_module.py
@@ -0,0 +1,38 @@
+"""Module fo creating admins."""
+
+class User():
+    """Create a new user."""
+
+    def __init__(self, first_name, last_name):
+        """Initiate new user."""
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+
+    def create_user(self):
+        """Add info about new user."""
+        print(f"{self.first_name} {self.last_name}")
+
+    def greet_user(self):
+        """Greeting user."""
+        print(f"Hi, {self.first_name}! \nThank you for registration, welcome)")
+
+
+class Privilegies():
+    """Create Privilegies for Admin class."""
+    def __init__(self, privilegies=[]):
+        self.privilegies = privilegies
+
+    def show_privilegies(self):
+        """Show admin's privilegies."""
+        print("Privilegies:")
+        for privilegie in self.privilegies:
+            print(f"* {privilegie};")
+
+
+class Admin(User):
+    """Class Admin extends class User."""
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privilegies = Privilegies()
+
+
---
+++ chapter_9_classes/new_admin.py
@@ -0,0 +1,7 @@
+from admin_module import User, Privilegies, Admin
+
+anakin_skyworker = Admin('Anakin', 'Skyworker')
+anakin_skyworker.privilegies.privilegies = ['can add post', 'can edit post', 'can delete post', 'can be a Dart Wader']
+
+anakin_skyworker.create_user()
+anakin_skyworker.privilegies.show_privilegies()
