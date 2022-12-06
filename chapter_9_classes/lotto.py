e3c2a72df6f3e95b69c5767cb9375f7406e55282
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-06 14:34:16 +0200

    update lotto

--- chapter_9_classes/lotto.py
+++ chapter_9_classes/lotto.py
@@ -3,9 +3,13 @@ from random import choice
 digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 characters = ['a', 'b', 'c', 'd', 'e']
 
-win_digit = choice(digits)
-win_character = choice(characters)
+first_number = choice(digits)
+first_character = choice(characters)
+second_number = choice(digits)
+second_character = choice(characters)
 
-print(f"The winner is {win_digit}-{win_character.title()}!")
+win_combination = f"{first_number}{first_character.title()}{second_number}{second_character.title()}"
+
+print(f"The winner is {win_combination}!")
 
 
