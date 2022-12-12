9f6633cde5f023216e861e26ae3aa39c191ab61e
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-12 19:33:20 +0200

    add birthday check in pi

--- chapter_10_files_and_exceptions/file_reader.py
+++ chapter_10_files_and_exceptions/file_reader.py
@@ -3,5 +3,17 @@ filename = 'pi_digits.txt'
 with open(filename) as file_object:
     lines = file_object.readlines()
 
+pi_string = ''
 for line in lines:
-    print(line.rstrip())
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears ion the first million digits of pi!")
+else:
+    print("You birthday does not appear in the first million digits of pi.")
+
+#output first 50 symbols after dot
+print(f"{pi_string[:52]}")
+print(len(pi_string))
+
