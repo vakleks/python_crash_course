e20bf176f5d4c6735876937c74a5142798e23be4
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-13 18:18:00 +0200

    update calculator

--- chapter_10_files_and_exceptions/division_calculator.py
+++ chapter_10_files_and_exceptions/division_calculator.py
@@ -1,4 +1,13 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't devide by zero!")
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nInput first number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nInput second number: ")
+    if second_number == 'q':
+        break
+answer = int(first_number) / int(second_number)
+print(f"Result is: {answer}")
+
