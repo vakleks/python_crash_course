1906cbf703abeed9be3ab05bc5587444588957ae
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-13 11:55:10 +0200

    replace Python with JavaScript

--- chapter_10_files_and_exceptions/learning_python.py
+++ chapter_10_files_and_exceptions/learning_python.py
@@ -24,4 +24,12 @@ for line in lines:
 
 print(python_opportunities)
 
+#replace programming language
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.replace('Python', 'JavaScript').rstrip())
 
