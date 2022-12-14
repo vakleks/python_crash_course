86ae76855222ae37f72e7ffeb205b4306703603a
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-14 20:20:47 +0200

    refactoring greet user

--- chapter_10_files_and_exceptions/remember_me.py
+++ chapter_10_files_and_exceptions/remember_me.py
@@ -1,15 +1,17 @@
 import json
 
-# Load user's name, if it already saved.
-# Else ask the user's name and save it.
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename, 'w') as f:
-        json.dump(username, f)
-        print(f"We'll remember you  when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+def greet_user():
+    """Greet user by the name."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"We'll remember you  when you come back, {username}!")
+    else:
+        print(f"Welcome back, {username}!")
+
+greet_user()
