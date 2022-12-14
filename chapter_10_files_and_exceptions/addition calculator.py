dba2512832a9e7de04b5be18de6c3f336d667b5f
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-14 17:54:14 +0200

    addition calculator

---
+++ chapter_10_files_and_exceptions/addition calculator.py
@@ -0,0 +1,17 @@
+print("Give me two numbers, and I'll add them.")
+print("Enter 'exit' to quit.")
+
+while True:
+    first_number = input("\nInput first number: ")
+    if first_number == 'exit':
+        break
+    second_number = input("\nInput second number: ")
+    if second_number == 'exit':
+        break
+    try:
+        answer = int(first_number) + int(second_number)
+    except ValueError:
+        print("You can't add text to number!")
+    else:
+        print(f"The sum is: {answer}")
+
