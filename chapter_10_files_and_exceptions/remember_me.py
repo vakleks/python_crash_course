98f1447b865ce0b543f76b746e5ea8d9ad611441
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-14 20:04:21 +0200

    add greet user

---
+++ chapter_10_files_and_exceptions/remember_me.py
@@ -0,0 +1,15 @@
+import json
+
+# Load user's name, if it already saved.
+# Else ask the user's name and save it.
+filename = 'username.json'
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll remember you  when you come back, {username}!")
+else:
+    print(f"Welcome back, {username}!")
