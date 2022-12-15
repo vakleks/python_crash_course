cf175f2a24898994fd8b2b5f6caadb7d02179e1b
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-15 18:03:11 +0200

    uodate test_name_function

--- chapter_11_code_testing/test_name_function.py
+++ chapter_11_code_testing/test_name_function.py
@@ -9,5 +9,10 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('john', 'doe')
         self.assertEqual(formatted_name, 'John Doe')
 
+    def test_first_last_middel_name(self):
+        """Does function work with names like 'Wolfgang Amadeus Mozart'?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 if __name__ == '__main__':
     unittest.main()
