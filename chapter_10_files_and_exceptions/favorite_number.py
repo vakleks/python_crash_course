79b69a6c166c76b59e26b005f2faf53537ceb39e
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-15 15:45:41 +0200

    refactored favorite_number

--- chapter_10_files_and_exceptions/favorite_number.py
+++ chapter_10_files_and_exceptions/favorite_number.py
@@ -1,8 +1,13 @@
 import json
 
-fav_number = input("What is your favorite number? ")
-
 filename = 'favorite_number.json'
-with open(filename, 'w') as f:
-    json.dump(fav_number, f)
-    print(f"Your favorite number is {fav_number}, I'll remember it!")
+try:
+    with open(filename) as f:
+        fav_number = json.load(f)
+except FileNotFoundError:
+    fav_number = input("What is your favorite number? ")
+    with open(filename, 'w') as f:
+        json.dump(fav_number, f)
+        print(f"I'll remember your favorite number!")
+else:
+    print(f"Yor favorite number is {fav_number}!")
