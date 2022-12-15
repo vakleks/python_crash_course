2e83ab1f92524df542ef18d9166ead6643d9115f
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-15 18:42:08 +0200

    update city_functions.py update test_cities.py

--- chapter_11_code_testing/city_functions.py
+++ chapter_11_code_testing/city_functions.py
@@ -1,7 +1,7 @@
 def full_city_info(city, country, population=''):
     """Create full info about the city."""
     if population:
-        full_info = f"{city}, {country}, population - {population}"
+        full_info = f"{city}, {country} - {population}"
     else:
         full_info = f"{city}, {country}" 
     return full_info.title()
--- chapter_11_code_testing/test_cities.py
+++ chapter_11_code_testing/test_cities.py
@@ -9,5 +9,10 @@ class CitiesTestCase(unittest.TestCase):
         city_info = full_city_info('santiago', 'chile')
         self.assertEqual(city_info, 'Santiago, Chile')
 
+    def test_city_country_population(self):
+        """Does function return full info about the city like 'Santiago, Chile - 5000000'?"""
+        city_info = full_city_info('santiago', 'chile', '5000000')
+        self.assertEqual(city_info, 'Santiago, Chile - 5000000')
+
 if __name__ == '__main__':
     unittest.main()
