d42bce9d4938e1f393be9802cc4c4bcbc3db16d1
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-15 15:22:32 +0200

    refactored remember_me

--- chapter_10_files_and_exceptions/remember_me.py
+++ chapter_10_files_and_exceptions/remember_me.py
@@ -11,17 +11,22 @@ def get_stored_username():
     else:
         return username
 
+def get_new_username():
+    """Ask user's name."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
 def greet_user():
     """Greet user by the name."""
     username = get_stored_username()
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        filename = 'username.json'
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come back, {username}!")
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
 
