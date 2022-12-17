88a29d87bd67357cb416430d17b1080944cae343
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-17 16:12:29 +0200

    add language_survay.py

---
+++ chapter_11_code_testing/language_survey.py
@@ -0,0 +1,18 @@
+from survey import AnonymysSurvey
+
+# Define the questions and create a survey.
+question = "What language did you first learn to speak?"
+my_survey = AnonymysSurvey(question)
+
+# Show questions and save answers to it.
+my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_reponse(response)
+
+# Show the results of survey.
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
