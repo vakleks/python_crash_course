24341d15a293b1ddb724ee1ea0f389672fe4bb2e
ovak <82042917+vakleks@users.noreply.github.com>
2022-12-20 19:13:15 +0200

    fix test_employee.py

--- chapter_11_code_testing/test_employee.py
+++ chapter_11_code_testing/test_employee.py
@@ -3,21 +3,21 @@ import unittest
 from employee import Employee
 
 class TestEmployee(unittest.TestCase):
-    """Tests for the module employee."""
+    """Tests for employee module."""
 
     def setUp(self):
-        """Make an employee to use in tests."""
-        self.eric = Employee('eric', 'matthes', 65_000)
+        """Create an employee for tests."""
+        self.johndoe = Employee('John', 'Doe', 20000)
 
     def test_give_default_raise(self):
-        """Test that a default raise works correctly."""
-        self.eric.give_raise()
-        self.assertEqual(self.eric.salary, 70000)
+        """Test for the default salary raise."""
+        self.johndoe.give_raise()
+        self.assertEqual(self.johndoe.salary, 25000)
 
     def test_give_custom_raise(self):
-        """Test that a custom raise works correctly."""
-        self.eric.give_raise(10000)
-        self.assertEqual(self.eric.salary, 75000)
+        """Test for the custom salary raise."""
+        self.johndoe.give_raise(10000)
+        self.assertEqual(self.johndoe.salary, 30000)
 
 if __name__ == '__main__':
     unittest.main()
